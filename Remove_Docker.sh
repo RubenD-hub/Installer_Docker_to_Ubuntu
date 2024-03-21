78f04b7f8823439546ac096bf2f4a33d87358198
RubenD-hub <rubenlop98@gmail.com>
2024-03-20 20:10:55 -0600

    Desinstalador

---
+++ Remove_Docker.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+clear
+msg="
+        ==================================================
+                Desinstalador de Docker y recursos                                    
+        ==================================================                                                
+"
+
+tput setaf 128;
+printf "$msg"
+tput setaf 7;
+
+printf "\n\n\tSe iniciara la Desinstalacion de Docker y Recursos\n\t"
+read -r -s -p "Presiona enter para continuar..."
+
+sleep 2 
+
