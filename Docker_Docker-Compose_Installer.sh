52a4ee6df5e536ed112d7680eee1398792870e71
RubenD-hub <rubenlop98@gmail.com>
2024-03-20 20:11:37 -0600

    Comando de actualizacion de paquetes

--- Docker_Docker-Compose_Installer.sh
+++ Docker_Docker-Compose_Installer.sh
@@ -10,9 +10,11 @@ tput setaf 128;
 printf "$msg"
 tput setaf 7;
 
-printf "\n\n\tSe iniciara la Instalacion de Docker\n\t"
+printf "\n\n\tSe iniciara la instalacion de docker\n\t"
 read -r -s -p "Presiona enter para continuar..."
 
 sleep 2
-# El siguiente comando elimina versiones de Docker previamente instaladas.
-# for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+printf  "$(tput setaf 214) \nSe actualizaran la lista de paquetes existentes \n"
+sudo apt update
+
